75357e0ae0c28d78143027027cb7681909d8fe5f
kip <cyruskiprop254@gmail.com>
2020-03-26 00:24:24 +0300

    add a restClient testing script

--- app/script/rest_requestor.rb
+++ app/script/rest_requestor.rb
@@ -2,13 +2,13 @@ require 'rest-client'
 
 puts 'Please enter the URL:'
 url = gets.chomp()
+base_url =  'http://localhost:3000/' + url
 
 puts 'Please enter the URL:'
 verb = gets.chomp()
 
-url 'http://localhost:3000'
 def rest_tester(url, verb)
-  RestClient.verb(url)
+  RestClient.get(url)
 end
 
-puts rest_tester(url, verb)
+puts rest_tester(base_url, verb)
